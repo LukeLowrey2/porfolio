6d2076c2737e24e528cea2878af475295c5eb864
LukeLowrey2 <89051599+LukeLowrey2@users.noreply.github.com>
2022-03-26 14:22:22 -0700

    styling, placement, place holders for project cards

--- src/App.js
+++ src/App.js
@@ -25,17 +25,36 @@ function App() {
         {/* Banner Image */}
       </div>
 
-      <main className="about">
-        <div >
-          <img className="profile-picture" src="https://i.imgur.com/1GDgwRP.jpg" alt="Luke Lowrey Profile"></img>
+      <div className="about">
         
-        </div>
-        <div className="about-me">
-          <p>
+          <img className="profile-picture" src="https://i.imgur.com/1GDgwRP.jpg" alt="Luke Lowrey Profile"></img>
+          
+         <div className="about-me">
+           <h3>
+             A Little About Me
+           </h3>
+          <p >
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nec ante a odio efficitur elementum. Nullam vel placerat lectus. Ut eleifend sodales ante, et luctus urna lobortis eget. Nullam vel lobortis erat. In gravida, nisi vel porttitor bibendum, lectus lorem mollis sem, quis cursus ex odio sed diam. Maecenas eget magna quam. Pellentesque a pellentesque nulla, id sodales sapien. Nulla laoreet risus et semper feugiat. Duis posuere justo at eros sollicitudin bibendum. Phasellus ornare, justo ut imperdiet rutrum, diam diam tincidunt elit, vitae luctus odio erat sed purus. Morbi malesuada risus non posuere dictum. Nunc turpis justo, venenatis sodales diam eget, venenatis vestibulum sapien. Morbi congue ipsum at tellus porttitor, quis eleifend est dictum. Donec ut tortor id orci sollicitudin congue. Vestibulum efficitur facilisis augue, quis consequat mi varius a.
+            
           </p>
+          </div>
+        
+      </div>
+
+      <div className="project">
+        <div className="project-single">
+        <p>Title</p>
+          <img className="place-holder" src="https://media.istockphoto.com/vectors/web-vector-id1162910018?k=20&m=1162910018&s=612x612&w=0&h=PeL2EHJBDY3hg3YH862SGPzvYiqKwAOOUcs7gd7jnUc="></img>
         </div>
-      </main>
+        <div className="project-single">
+        <p>Title</p>
+          <img className="place-holder" src="https://media.istockphoto.com/vectors/web-vector-id1162910018?k=20&m=1162910018&s=612x612&w=0&h=PeL2EHJBDY3hg3YH862SGPzvYiqKwAOOUcs7gd7jnUc="></img>
+        </div>
+        <div className="project-single">
+        <p>Title</p>
+          <img className="place-holder" src="https://media.istockphoto.com/vectors/web-vector-id1162910018?k=20&m=1162910018&s=612x612&w=0&h=PeL2EHJBDY3hg3YH862SGPzvYiqKwAOOUcs7gd7jnUc="></img>
+        </div>
+      </div>
 
     </div>
   );
