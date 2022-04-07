01549ae827e08747b66b4a210533812ce6f1bf7e
LukeLowrey2 <89051599+LukeLowrey2@users.noreply.github.com>
2022-04-07 15:42:40 -0700

    Updated css and lay out, created components

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
             A Little About Me
           </h3>
           <p >
-          Throughout my life I’ve found joy and magic when creating or editing websites with code. Be it making my Myspace page fit my everchanging personality as a kid, expanding my knowledge of design and responsiveness in my Interactive Media Program, or building a graphic design portfolio to show off my work. I always found coding to be spectacular and rewarding. After learning the tools to create functionality between the front and back end of an application, I’m excited to see where my future will take me. A future geared toward helping and expanding my community and the world through web development. 
+            Throughout my life I’ve found joy and magic when creating or editing websites with code. Whether it be making my Myspace page fit my ever-changing personality as a kid, expanding my knowledge of design and responsiveness in my Interactive Media Program, or building a graphic design portfolio to show off my work. I always found coding to be spectacular and rewarding. After learning the tools to create functionality between the front and back end of an application, I’m excited to see where my future will take me: a future geared toward helping and expanding my community and the world through web development.
 
           </p>
         </div>
@@ -46,27 +46,56 @@ function App() {
       <div className="project">
 
         <div className="project-single">
-          <h3>this.Group Records</h3>
+        <h3>this.Group Records</h3>
+
+          
+          
+          <img className="project-picture" src="https://i.imgur.com/kJwgMKm.png"></img>
+
+          
           <p>E-commerce site using React, Node.js, and Express. Users are able to create a login to add records to their cart and checkout. Backend constructed with SQL, and PostgreSQL.</p>
-          <a href="https://github.com/this-Group/graceShopperFrontEnd" target="_blank"><img className="project-picture" src="https://i.imgur.com/kJwgMKm.png"></img></a>
+          <a href="https://github.com/this-Group/graceShopperFrontEnd" target="_blank">GitHub</a>
         </div>
         <br></br>
+
         <div className="project-single">
-          <h3>Stranger's Things</h3>
+        <h3>Stranger's Things</h3>
+          
+           
+            <img className="project-picture" src="https://i.imgur.com/AWbjChI.png"></img>
+          
+          
           <p>React application using routes to an API for user interaction. When logged in users can post, update, and delete ads of their own. Users can also message other users about ads. </p>
-          <a href="https://github.com/LukeLowrey2/strangers_things" target="_blank"><img className="project-picture" src="https://i.imgur.com/AWbjChI.png"></img></a>
+          <a href="https://github.com/LukeLowrey2/strangers_things" target="_blank">GitHub</a>
+        </div>
+
+      </div>
+
+
+      <div className="project">
+
+        <div className="project-single">
+        <h3>Fitness Tracker</h3>
+        <img className="project-picture" src="https://i.imgur.com/vQXrNGm.png"></img>
+          
+            
+          
+          
+          <p>SPA using React and routes to an API. Users can login and collect work out routines made by other users or create their own. These routines are comprised of activities which are create by users.</p>
+          <a href="https://github.com/LukeLowrey2/Fitness_Trackr" target="_blank">GitHub</a>
+
         </div>
         <br></br>
         <div className="project-single">
-          <h3>Fitness Tracker</h3>
-          <p>SPA using React and routes to an API. Users can login and collect work out routines made by other users or create their own. These routines are comprised of activities which are create by users.  </p>
-          <a href="https://github.com/LukeLowrey2/Fitness_Trackr" target="_blank"><img className="project-picture" src="https://i.imgur.com/vQXrNGm.png"></img></a>
-        </div>
-        <br></br>
-        <div className="project-single">
-          <h3>Arcade Game</h3>
-          <p>Interactive game using JavaScript, HTLM, and CSS. Players can assign their names to the board and play until l there is a winner or a draw. If players want to continue, they have the ability to reset the board with the rest button.  </p>
-          <a href="https://github.com/LukeLowrey2/Arcade" target="_blank"><img className="project-picture" src="https://i.imgur.com/BBKpwV2.png"></img></a>
+        <h3>Arcade Game</h3>
+        <img className="project-picture" src="https://i.imgur.com/BBKpwV2.png"></img>
+          
+            
+          
+         
+          <p>Interactive game using JavaScript, HTLM, and CSS. Players can assign their names to the board and play until a win or a draw. If players want to continue, they have the ability to reset the board with the rest button.  </p>
+          <a href="https://github.com/LukeLowrey2/Arcade" target="_blank">GitHub</a>
+
         </div>
         <br></br>
       </div>
@@ -83,7 +112,7 @@ function App() {
           <p><b>Interactive Media Certificate - </b><em>San Diego Continuing Education</em></p>
           <p><b>Bachelor of Science in Mass Communication - </b><em>University of West Georgia, Carrollton, GA</em></p>
         </div>
-        
+
 
         <div>
           <h2><strong>Experience</strong></h2>
